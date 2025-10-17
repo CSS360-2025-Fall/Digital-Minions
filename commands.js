d79b722756584336f4473f34bc81421d2494f79e
omsoul <omever@uw.edu>
2025-10-17 08:27:56 -0700

    Modified commands.js folder to include /rules command

--- commands.js
+++ commands.js
@@ -44,6 +44,15 @@ const CHALLENGE_COMMAND = {
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+// Rules command
+const RULES_COMMAND = {
+    name: 'rules',
+    description: 'Explains the rules  of the game',
+    type: 1,
+    integration_types: [0, 1],
+    contexts: [0, 1, 2],
+};
+
+const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND, RULES_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
