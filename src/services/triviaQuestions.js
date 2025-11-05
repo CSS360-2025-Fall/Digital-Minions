cb015be437da070ad7a10c0d714173e84a8bc6f1
Johnny <jrr67@uw.edu>
2025-11-04 19:09:44 -0800

    Create triviaQuestions with math category
    Added a triviaQuestions object with math questions and options.

---
+++ src/services/triviaQuestions.js
@@ -0,0 +1,60 @@
+export const triviaQuestions = {
+  //questions difficulty will vary to aid in optional future operations on adding difficulty to the bot. Each category will contain 10 questions, though.
+  math: [
+    {
+      question: "What is 11 x 15?",
+      options: ["175", "140", "165", "160"],
+      correct: "165",
+    },
+    {
+      question: "What is 7 factorial(7!)?",
+      options: ["5,000", "5,500", "5,050", "5,040"],
+      correct: "5,040",
+    },
+    {
+      question: "What is the smallest positive number that is both a square and a cube?",
+      options: ["2", "3", "1", "0"],
+      correct: "1",
+    },
+    {
+      question: "What is the Roman numeral for 100?",
+      options: ["L", "C", "D", "M"],
+      correct: "C",
+    },
+    {
+      question: "What is the square root of 529?",
+      options: ["24", "20", "23", "21"],
+      correct: "23",
+    },
+    {
+      question: "What is the only even prime number?",
+      options: ["2", "4", "0", "6"],
+      correct: "2",
+    },
+    {
+      question: "How many prime numbers are between 1 and 20?",
+      options: ["4", "10", "9", "8"],
+      correct: "8",
+    },
+    {
+      question: "What is the next prime number after 97?",
+      options: ["99", "101", "153", "103"],
+      correct: "101",
+    },
+    {
+      question: "How many sides does a hexagon have?",
+      options: ["5", "6", "7", "8"],
+      correct: "6",
+    },
+    {
+      question: "What type of triangle has sides 3,4, and 5?",
+      options: ["Isosceles", "Equilateral", "Right", "Scalene"],
+      correct: "Right",
+    },
+  ],
+  history: [
+    {
+      question: "?",
+      options: ["", "", "", ""],
+      correct: "",
+    },
