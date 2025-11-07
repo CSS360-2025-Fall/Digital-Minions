c5c0b1729180933cf64c2f4389e3482997bc327a
Johnny <jrr67@uw.edu>
2025-11-07 12:24:22 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -35,5 +35,5 @@ app.get('/', (req, res) => {
 // ←←← LOCAL ONLY: HARD-CODED PORT 3000
 app.listen(3000, () => {
   console.log('Listening on port 3000');
-  announceStartup();
+  //announceStartup();
 });
