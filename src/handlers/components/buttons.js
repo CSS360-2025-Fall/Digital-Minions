1ea3a5dc31f129e57e6ce500961b846bae6f7481
Johnny <jrr67@uw.edu>
2025-11-07 12:57:23 -0800

    Update buttons.js

--- src/handlers/components/buttons.js
+++ src/handlers/components/buttons.js
@@ -3,13 +3,13 @@ import { COMPONENT_IDS } from '../../constants/index.js';
 import { discordRequest } from '../../utils/discord.js';
 import { createWebhookEndpoint } from '../../utils/helpers.js';
 import { createChoiceSelectionMessage } from '../../utils/messageBuilders.js';
-import { getShuffledOptions } from '../../services/game.js';
+//import { getShuffledOptions } from '../../services/game.js';
 
 /**
  * Handles the accept button click
  * Deletes the challenge message and sends an ephemeral choice menu
  */
-export async function handleAcceptButton(req, res) {
+/**export async function handleAcceptButton(req, res) {
   const { data, message } = req.body;
   const componentId = data.custom_id;
   const gameId = componentId.replace(COMPONENT_IDS.ACCEPT_BUTTON, '');
@@ -31,4 +31,4 @@ export async function handleAcceptButton(req, res) {
   } catch (err) {
     console.error('Error handling accept button:', err);
   }
-}
+}*/
