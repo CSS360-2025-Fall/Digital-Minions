b38fb20cbf98403474c2e4b1d7e18a1a6c66c07a
Johnny <jrr67@uw.edu>
2025-11-07 09:36:53 -0800

    Update commands.js

--- src/commands.js
+++ src/commands.js
@@ -1,7 +1,10 @@
 // src/commands.js
 import 'dotenv/config';
 import { discordRequest } from './utils/discord.js';
-import { COMMANDS } from './config/commands.js';
+import COMMANDS from './config/commands.js';  // ← CHANGED TO DEFAULT IMPORT
+
+// If the above still fails, try this instead (uncomment if needed):
+// const COMMANDS = require('./config/commands.js');
 
 // Register all slash commands globally
 async function installGlobalCommands() {
@@ -9,7 +12,10 @@ async function installGlobalCommands() {
 
   try {
     console.log('Started refreshing global application (/) commands...');
-    await discordRequest(endpoint, { method: 'PUT', body: Object.values(COMMANDS) });
+    await discordRequest(endpoint, { 
+      method: 'PUT', 
+      body: Object.values(COMMANDS)  // ← Works for both default and named
+    });
     console.log('Successfully reloaded global application (/) commands.');
   } catch (err) {
     console.error('Error installing commands:', err);
