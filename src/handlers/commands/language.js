5c7387f0d893edffa69681fb78db671f4b97b5f4
omsoul <omever@uw.edu>
2025-11-19 17:20:27 -0800

    Adding language command handler

---
+++ src/handlers/commands/language.js
@@ -0,0 +1,22 @@
+﻿import { InteractionResponseType } from 'discord-interactions';
+import { extractUserId } from '../../utils/helpers.js';
+import { createSimpleMessage } from '../../utils/messageBuilders.js';
+import { setUserLocale } from '../../services/gameState.js';
+
+export async function handleLanguageCommand(req, res) {
+    const userId = extractUserId(req);
+    const locale = req.body.data.options[0].value;
+
+    setUserLocale(userId, locale);
+
+    const confirmationMessages = {
+        en: '✅ Language set to English',
+        es: '✅ Idioma configurado a Español',
+        fr: '✅ Langue définie en Français',
+    };
+
+    return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: createSimpleMessage(confirmationMessages[locale] || confirmationMessages.en, true),
+    });
+}
